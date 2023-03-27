3d2063c27b956562b84c47934f1e1bcff070e409
bhdgogogo <1245874479@qq.com>
2023-03-27 16:07:34 +0800

    静态页面代码上传

--- docs/.vitepress/examples/spin/spin3.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="box">
-    <sb-spin :size="36" dot />
-  </div>
-</template>
--- docs/.vitepress/examples/spin/spin4.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="box">
-    <sb-spin :size="36" tip="This may take a while..." />
-  </div>
-</template>
