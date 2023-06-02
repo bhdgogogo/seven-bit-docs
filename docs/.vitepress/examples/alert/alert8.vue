866a6a135d0129b2b4a2cbce283ba97a8fb28140
bhdgogogo <1245874479@qq.com>
2023-06-02 15:47:08 +0800

    feat

--- docs/.vitepress/examples/alert/alert8.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="box">
-        <sb-alert banner>This is an info alert.</sb-alert>
-    </div>
-</template>
--- docs/.vitepress/examples/spin/spin1.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="box">
-    <sb-spin />
-  </div>
-</template>
